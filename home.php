9fe161bfd8c098e7002139769270db113fc321e6
Justin Tallant <jtallant07@gmail.com>
2013-03-05 00:21:30 -0600

    custom home page

---
+++ home.php
@@ -0,0 +1,71 @@
+<?php
+
+add_filter('genesis_pre_get_option_site_layout', '__genesis_return_full_width_content');
+add_action('genesis_meta', 'picture_perfect_home_genesis_meta');
+/**
+ * Add widget support for homepage. If no widgets active, display the default loop.
+ *
+ * @return void
+ */
+function picture_perfect_home_genesis_meta()
+{
+	$sidebar_widget_areas = array(
+		'home-right',
+	);
+
+	if (false === any_picture_perfect_sidebar_is_active($sidebar_widget_areas)) {
+		return;
+	}
+
+	# Custom body class
+	add_filter('body_class', 'picture_perfect_custom_home_page_body_class');
+
+	# Remove the loop and add a custom loop
+	remove_action('genesis_loop', 'genesis_do_loop');
+	add_action('genesis_loop', 'picture_perfect_home_loop_helper');
+}
+
+
+/**
+ * Adds a custom body class on the home page
+ *
+ * @param array $classes the current body classes
+ *
+ * @return array modified classes
+ */
+function picture_perfect_custom_home_page_body_class($classes)
+{
+   $classes[] = 'custom-home-page';
+   return $classes;
+}
+
+
+/**
+ * Display widget content for homepage sections
+ */
+function picture_perfect_home_loop_helper()
+{
+	if (is_active_sidebar('home-right')) {
+		dynamic_sidebar('home-right');
+	}
+}
+
+/**
+ * Returns true if any of the picture_perfect sidebars are active
+ *
+ * @param array $sidebar_widget_areas registered sidebars for use on home page
+ *
+ * @return bool
+ */
+function any_picture_perfect_sidebar_is_active($sidebar_widget_areas)
+{
+	foreach($sidebar_widget_areas as $sidebar) {
+		if ( is_active_sidebar($sidebar) ) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+genesis();
