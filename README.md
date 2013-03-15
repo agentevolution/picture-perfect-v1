3df13bff6547b67ec6d1e0b047a4438bc6a17e80
Justin Tallant <jtallant07@gmail.com>
2013-03-15 10:08:43 -0400

    default image, css changes, compile css

--- README.md
+++ README.md
@@ -5,13 +5,7 @@ Genesis child theme by Agent Evolution
 
 ## NOTES
 
-Decide on method for loading background image...
-Probably use jQuery and load a background-image with a smaller size
-on smaller screen sizes. This method will avoid loading giant images
-on smart phones.
 
-Make sure that background is fixed on smart phones as the layout will be
-extremely verticle causing a terrible stretch of the background image.
 
 
 ## TODO
@@ -19,11 +13,14 @@ extremely verticle causing a terrible stretch of the background image.
 * Some kind of "Setup Theme" function that runs migrations (puts widgets in widget areas, etc.)?
 * Add gravity forms form to contact page
 * Dynamically size and center any logo given the original dimensions
+* Decide on method for loading background image. Probably use jQuery and load a background-image with a smaller size on smaller screen sizes. This method will avoid loading giant images on smart phones. Make sure that background is fixed on smart phones as the layout will be extremely verticle causing a terrible stretch of the background image.
+* CSS for AgentPress Listings
 
 
+## Design changes
+* No borders on nav items. The nav looked a little weird with borders when the items start collapsing down on smaller screen sizes.
 
 ## Genesis-child TODO
-
 * Remove .related-posts css
 * Go through all CSS and rip out AE Client site specific stuff
 * home.php
@@ -35,6 +32,6 @@ extremely verticle causing a terrible stretch of the background image.
 As far as coding style goes, genesis-child leans more toward [FIG Standards](http://www.php-fig.org/) (with some exceptions) over WordPress coding standards. This is because the majority of popular PHP frameworks and CMS's compyl entirely or more closely to FIG standards. WordPress coding standards seem a little outdated and the spacing between every function call is annoying. It makes more sense to adhere to the PHP community at large. That way you don't have to change the way you code on a per project basis.
 
 
-## List of things to style for
+## List of things to style for as a standard for all themes
 * Genesis Nav Extras
 * Agent Listings (StudioPress)
--- lib/classes/class-agentevo-customizer.php
+++ lib/classes/class-agentevo-customizer.php
@@ -17,7 +17,7 @@ if (defined('ABSPATH') === false) {
 add_action( 'customize_register'    , array( 'Agentevo_Theme_Options', 'register'            ) );
 // add_action( 'init'                  , array( 'Agentevo_Theme_Options', 'remove_all_mods'     ) );
 // add_action( 'init'                  , array( 'Agentevo_Theme_Options', 'remove_section_mods' ) );
-// add_action( 'wp_head'               , array( 'Agentevo_Theme_Options', 'render'              ) );
+add_action( 'wp_head'               , array( 'Agentevo_Theme_Options', 'render'              ) );
 /**
  * Contains methods for customizing the theme customization screen.
  *
@@ -34,8 +34,110 @@ class Agentevo_Theme_Options
 	 * @see add_action('customize_register', $func)
 	 * @param WP_Customize_Manager $wp_customize
 	 */
-	public static function register( $wp_customize )
+	public static function register($wp_customize)
 	{
-		
+		# Sections
+		# ==========================================================================
+
+		$wp_customize->add_section(
+			'general_settings',
+			array(
+				'title'    => __('General Settings', 'agentevo'),
+				'priority' => 35,
+			)
+		);
+
+
+		# Settings & Controls
+		# ==========================================================================
+
+
+		/* General Settings
+		-----------------------------------------------*/
+
+		$wp_customize->add_setting(
+			'default_background_image',
+			array(
+				'default' => get_stylesheet_directory_uri() . '/images/PicturePerfect.jpg'
+			)
+		);
+
+		$wp_customize->add_control(
+			new WP_Customize_Image_Control(
+				$wp_customize,
+				'default_background_image',
+				array(
+					'label'    => __('Default background image', 'agentevo'),
+					'section'  => 'general_settings',
+					'priority' => 1
+				)
+			)
+		);
+	}
+
+
+	/**
+	 * This will output the custom WordPress settings to the theme's WP head.
+	 *
+	 * Used by hook: 'wp_head'
+	 *
+	 * @see add_action('wp_head',$func)
+	 */
+	public static function render()
+	{
+		?>
+		<!-- BEGIN CUSTOMIZER CSS -->
+		<style>
+		<?php
+		self::generate_css(
+			'body',
+			'background',
+			'default_background_image',
+			'url(',
+			') no-repeat center center fixed'
+		);
+		?>
+		</style>
+		<?php
+	}
+
+
+	/**
+	 * Generates a line of CSS for use in header output.
+	 *
+	 * If the setting ($mod_name) has no defined value, the CSS will not be output.
+	 *
+	 * @uses get_theme_mod()
+	 *
+	 * @param string $selector CSS selector
+	 * @param string $style The name of the CSS property to modify
+	 * @param string $mod_name The name of the theme_mod option to fetch
+	 * @param string $prefix Optional. Anything that needs to be output before the CSS property
+	 * @param string $postfix Optional. Anything that needs to be output after the CSS property
+	 * @param bool $echo Optional. Whether to print directly to the page (default: true).
+	 *
+	 * @return string Returns a single line of CSS with selectors and a property.
+	 */
+	public static function generate_css($selector, $style, $mod_name, $prefix='', $postfix='', $echo=true)
+	{
+		$mod = get_theme_mod($mod_name);
+
+		if ( empty($mod) ) {
+			return '';
+		}
+
+		$output = sprintf('%s { %s:%s; }',
+			$selector,
+			$style,
+			$prefix . $mod . $postfix
+		);
+
+		$output .= "\n";
+
+		if ( $echo ) {
+			echo $output;
+		}
+
+		return $output;
 	}
 }
