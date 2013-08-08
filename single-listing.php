58cf077d2778379e7740c7838f7163a6015ea07e
Dave Bonds <dave@agentevolution.com>
2013-08-08 11:44:38 -0500

    loop hook update on single listing

--- single-listing.php
+++ single-listing.php
@@ -1,6 +1,6 @@
 <?php
-remove_action( 'genesis_before_post_content', 'genesis_post_info' );
-remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
-remove_action( 'genesis_after_post', 'genesis_do_author_box_single' );
+remove_action( 'genesis_entry_header', 'genesis_post_info' );
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+remove_action( 'genesis_after_entry', 'genesis_do_author_box_single' );
 
 genesis();
